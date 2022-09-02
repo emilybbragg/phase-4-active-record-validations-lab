ac901b8ca3a84979e8c360b3021c3044b92bd719
Emily <emilybribragg@gmail.com>
2022-09-02 10:50:24 -0400

    commit tree 79244d6178170e8d75789ded4a4647e89477fbc2

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,8 @@
 class Author < ApplicationRecord
+
+# All authors have a name; no two authors have the same name
+  validates :name, presence: true, uniqueness: true
+
+# Author phone numbers are exactly ten digits
+  validates :phone_number, length: { is: 10 }
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,27 @@
 class Post < ApplicationRecord
+
+# All posts have a title
+validates :title, presence: true
+# Post content is at least 250 characters long
+validates :content, length: { minimum: 250 } 
+# Post summary is a maximum of 250 characters
+validates :summary, length: { maximum: 250 } 
+# Post category is either Fiction or Non-Fiction
+validates :category, inclusion: {in: ['Fiction', 'Non-Fiction']}
+# add a custom validator to the Post model that ensures the title is sufficiently clickbait-y
+validate :clickbait?
+
+CLICKBAIT_PATTERNS = [
+    /Won't Believe/i,
+    /Secret/i,
+    /Top \d/i,
+    /Guess/i
+  ]
+
+  def clickbait?
+    if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+      errors.add(:title, "must be clickbait")
+    end
+  end
+
 end
